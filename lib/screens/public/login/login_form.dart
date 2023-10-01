298e015ca2c21dc83451e952bfbeb9799a1d0f80
Bohdan Ryzhko <103310223+bohdan-ryzhko@users.noreply.github.com>
2023-10-01 22:27:29 +0300

    remove comments

--- lib/screens/public/login/login_form.dart
+++ lib/screens/public/login/login_form.dart
@@ -1,20 +1,5 @@
 import 'package:financial_transactions/components/components.dart';
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-
-// class Auth {
-//   Dio dio = Dio();
-//   String baseUrl = 'http://localhost:8000/api/auth';
-
-//   Future<void> login(data) async {
-//     try {
-//       final response = await dio.post("$baseUrl/login", data: data);
-//       debugPrint(response.toString());
-//     } catch (error) {
-//       debugPrint(error.toString());
-//     }
-//   }
-// }
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -38,16 +23,6 @@ class LoginFormState extends State<LoginForm> {
     };
 
     debugPrint(loginInfo.toString());
-    // Auth().login(loginInfo);
-
-    // try {
-    //   final response =
-    //       await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/ditto'));
-
-    //   debugPrint(response.toString());
-    // } catch (error) {
-    //   debugPrint(error.toString());
-    // }
   }
 
   @override
